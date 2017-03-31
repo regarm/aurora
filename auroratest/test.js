98a7d0fa33c1b70d774253699b215f16da8ba4fc
Manoj Kumar Regar <regarmanojkumar@gmail.com>
2017-04-01 02:19:30 +0530

    Testing area.

--- auroratest/test.js
+++ auroratest/test.js
@@ -6,7 +6,7 @@
 
 var request = require('request');
 
-request.post({url: 'http://127.0.0.1:3000/api/jdfhf/contestExistance'}, function (err, res, body){
+request.post({url: 'http://127.0.0.1:5000/api/fetchFile/58b9db32dd221c0cb0f69ba4'}, function (err, res, body){
 	console.log(err);
 	console.log(body);
 });
