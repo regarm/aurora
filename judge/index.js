1d3f3c8381c82ee208dd8ee796e26d3a41ad3143
Manoj Kumar Regar <regarmanojkumar@gmail.com>
2017-04-01 02:23:28 +0530

    Message parsing

--- judge/index.js
+++ judge/index.js
@@ -1,42 +1,36 @@
-var WS = require('ws');
-var https = require('https');
-var fs = require('fs');
-var judge = require('./judge');
+var WS    = require('ws');
+var messenger = require('./lib/messenger');
 
+//Creating New Websocket Server
 var wss = new WS.Server({
   perMessageDeflate: false,
   port: 4091
 });
 
 
+//Event emitted on a new Connection
 wss.on('connection', function connection(ws){
-	/*
-	Emitted when the handshake is complete. socket is an instance of WebSocket.
-	*/
+
+	//Event emitted on new incoming message
 	ws.on('message', function incoming(message){
 		msg = JSON.parse(message);
-		if(msg.type === 'judge'){
-			ws.emit('judge', message);
-		}
+		messenger.process(ws, msg);
 	})
-	ws.on('judge', function incoming(message){
-		var msg = JSON.parse(message);
-		judge.run(msg.submission);
-	})
+
 });
+
+//Event emitted on error occurence.
 wss.on('error', function error(err){
 	console.log('Following error occured: ');
 	console.log(err);
 })
+
+
+//Emitted before the response headers are written to the socket as part of the handshake.
 wss.on('headers', function (headers){
-	/*
-		Emitted before the response headers are written to the socket as part of the handshake.
-		 This allows you to inspect/modify the headers before they are sent.
-	*/
-	// console.log('Headers are here : ', headers);
 })
+
+// Emitted when the underlying server has been bound.
 wss.on('listening', function (){
-	/* Emitted when the underlying server has been bound.*/
-	// console.log(wss);
 	console.log("Judge is listening\n");
 })
